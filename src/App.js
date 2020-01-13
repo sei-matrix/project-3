7f3bbec350bf22b7c717353173f9d014a390c2c1
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-01-13 16:32:18 +0300

    edited app.js

--- src/App.js
+++ src/App.js
@@ -16,23 +16,40 @@ class App extends Component {
       "Italian",
       "Chinese"
     ],
+    offsets:[5,0,0,5,0,5],
     cuisineInfo: [],
-    cuisineCard: []
+    recipes: []
   };
+
+  onCuisinechoice(name){
+
+    let recipesCpy = this.state.recipes
+    let url = `https://api.spoonacular.com/recipes/search?apiKey=01773742fd534e77967f3c3e59b214e6&cuisine=${name}&offset=${Math.floor(Math.random()*50)}&number=10`;
+    axios.get(url).then(res=>{
+      res.data.results.forEach(recipe=>{
+        recipesCpy.push(<Recipe recipe={recipe} />)
+      })
+   })
+          .then(()=>{
+            this.setState({recipes: recipesCpy})
+          })
+
+          return (this.state.recipes)
+  }
 
   componentDidMount() {
     let cuisine = "";
     let cuisineInfoCpy = [...this.state.cuisineInfo];
-    this.state.cuisineName.forEach(c => {
+    this.state.cuisineName.forEach((c,index) => {
       cuisine = c;
-      let url = `https://api.spoonacular.com/recipes/search?apiKey=01773742fd534e77967f3c3e59b214e6&cuisine=${cuisine}&offset=0&number=1`;
+      let url = `https://api.spoonacular.com/recipes/search?apiKey=01773742fd534e77967f3c3e59b214e6&cuisine=${cuisine}&offset=${this.state.offsets[index]}&number=1`;
       axios
         .get(url)
         .then(obj => {
           cuisine = c;
           cuisineInfoCpy.push(
             <Cuisine
-              key={obj.data.results[0].id}
+              key={index}
               name={cuisine}
               id={obj.data.results[0].id}
             />
@@ -43,13 +60,14 @@ class App extends Component {
         });
     });
 
-    console.log(this.state.cuisineInfo);
+    console.log(this.props);
   }
 
   render() {
     return (
       <div className="App">
         <Route path="/" exact render={() => this.state.cuisineInfo} />
+        <Route path="/cuisines/:id" exact component={Recipe}  />
       </div>
     );
   }
