f3abb4fb183f9c921ad3e717fc5b91dc1ed9e1b3
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-01-14 14:06:33 +0300

    fixed path issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import axios from "axios";
 import Recipes from "./components/recipes/Recipes";
 import Details from "./components/details/Details";
-import Nav from './components/navbar/Navbar'
+import Nav from './components/Navbar'
 import Header from './components/recipe/Header'
 import "./App.css";
 
@@ -51,8 +51,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+         <Route path="/" component={Nav} />
         <Route path="/" component={Header} />
-        <Route path="/" component={Nav} />
         <Route path="/" exact render={() => this.state.cuisineInfo} />
         <Route path="/cuisines/:id" exact component={Recipes} />
       </div>
