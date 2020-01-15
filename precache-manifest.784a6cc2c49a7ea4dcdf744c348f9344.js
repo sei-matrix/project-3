eb1554bfed6b4543887810d245b6944e0cdedee6
ahmedalnemer <ahmedalnemer3@gmail.com>
2020-01-15 16:44:10 +0300

    Updates

---
+++ precache-manifest.784a6cc2c49a7ea4dcdf744c348f9344.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82316aae6d6b7aba5c4d0b7d4e00c9dc",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "bf4b25948d967cb1729c",
+    "url": "/project-3/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "4e30e1a17ac79e891fd9",
+    "url": "/project-3/static/css/main.0ac1cc01.chunk.css"
+  },
+  {
+    "revision": "bf4b25948d967cb1729c",
+    "url": "/project-3/static/js/2.952e3978.chunk.js"
+  },
+  {
+    "revision": "39f2950d1dd4f4f4451dc4186e605ec7",
+    "url": "/project-3/static/js/2.952e3978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e30e1a17ac79e891fd9",
+    "url": "/project-3/static/js/main.ed3c60cc.chunk.js"
+  },
+  {
+    "revision": "e145e9d0bf553d0388c2",
+    "url": "/project-3/static/js/runtime-main.0fa09ced.js"
+  }
+]);
