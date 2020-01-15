d6b5f19ca9e359da46b38b9817dfab9aa9e74071
maysa9 <mayar.a.saleh@outlook.com>
2020-01-15 10:23:06 +0300

    Print button components

---
+++ src/components/print/Print.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Print extends Component {
+    state = {  }
+    handelChange(){
+
+    }
+    render()
+     { 
+        return ( 
+            <div>
+                <button onClick='handelChange'>
+                Print 
+                </button>
+            </div>
+         );
+    }
+}
+ 
+export default Print;
